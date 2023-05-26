4f0626b183ce1e0ac87d431b0ebc2c79de3b37b5
shayanthekid <sajidfayazhaniff@gmail.com>
2023-05-26 17:20:31 +0530

    added amplify get all cars

--- amplify/backend/function/GetAllCars/src/index.js
+++ amplify/backend/function/GetAllCars/src/index.js
@@ -1,17 +1,59 @@
 
 
+
+const AWS = require('aws-sdk');
+
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
+
 exports.handler = async (event) => {
-    console.log(`EVENT: ${JSON.stringify(event)}`);
-    return {
-        statusCode: 200,
-    //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*",
-    //      "Access-Control-Allow-Headers": "*"
-    //  },
-        body: JSON.stringify('testing123'),
+    // Add CORS headers
+    const headers = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
     };
+
+    // Check if the request is an OPTIONS preflight request
+    if (event.httpMethod === 'OPTIONS') {
+        return {
+            statusCode: 200,
+            headers: headers,
+        };
+    }
+
+    // Rest of your code to retrieve cars from DynamoDB
+    const params = {
+        TableName: 'carpool',
+    };
+
+    try {
+        const result = await dynamoDB.scan(params).promise();
+
+        if (result.Items && result.Items.length > 0) {
+            return {
+                statusCode: 200,
+                headers: headers,
+                body: JSON.stringify(result.Items),
+            };
+        } else {
+            return {
+                statusCode: 404,
+                headers: headers,
+                body: JSON.stringify({ message: 'No cars found' }),
+            };
+        }
+    } catch (error) {
+        console.error('Error retrieving cars:', error);
+
+        const errorMessage = 'Failed to retrieve cars';
+
+        return {
+            statusCode: 500,
+            headers: headers,
+            body: JSON.stringify({ message: errorMessage, error: error.stack }),
+        }
+    }
 };
